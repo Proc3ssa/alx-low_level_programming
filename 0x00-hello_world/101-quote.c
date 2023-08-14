677353b1ba00725124285d9961ece68c3421c778
Processor <Pros3sa@gmail.com>
2023-08-14 20:50:29 +0000

    tried the corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,17 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return 1;
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message to the standard error.
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
+
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the message
+ * "Programming is like building a multilingual puzzle,"
+ * using the puts function and returns 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     puts("Programming is like building a multilingual puzzle,");
-    return 0;
+    return (0);
 }
+
