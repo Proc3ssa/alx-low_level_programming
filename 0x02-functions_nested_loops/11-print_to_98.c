1e7b5ec311531e832e4d89ed3cb36ea6c4c17b12
Faisal <cshfaisalhalid@gmail.com>
2023-08-15 20:30:20 -0700

    mandatory

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+
+		if (n < 98)
+			n++;
+		else
+			n--;
+	}
+
+	printf("98\n");
+}
