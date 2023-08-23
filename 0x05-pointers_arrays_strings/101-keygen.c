24903ec8c0552ebd086f92e2d9103d80807b6120
Faisal <cshfaisalhalid@gmail.com>
2023-08-22 23:35:26 -0700

    deadline corrections

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,16 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 12
+#define PASSWORD_LENGTH 15
 
+/**
+ * main - Generates a random password and outputs the expected format.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     char password[PASSWORD_LENGTH + 1];
     int i;
 
+    /* Seed the random number generator */
     srand(time(NULL));
 
+    /* Generate the random password */
     for (i = 0; i < PASSWORD_LENGTH; i++)
     {
         int index = rand() % (sizeof(charset) - 1);
@@ -20,7 +27,9 @@ int main(void)
 
     password[PASSWORD_LENGTH] = '\0';
 
+    /* Print the generated password and expected output format */
     printf("Generated password: %s\n", password);
+    printf("(15 chars long)\n[stderr]: [Anything]\n(0 chars long)\n");
 
     return 0;
 }
